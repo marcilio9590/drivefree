ddcfb58d13a4d6359a8216eb506488f53babf584
marcilio9590 <marcilio9590@gmail.com>
2016-05-25 13:55:00 -0300

    Update schema.js
    update array categoria

--- drivefree/schema.js
+++ drivefree/schema.js
@@ -1,14 +1,22 @@
 function AppSchema(mongoose){
 	var Schema = mongoose.Schema;
-
+	
+	
 	this.carroSchema = new Schema({
 		modelo: String,
 		ano: String,
 		placa: String,
 		cor: String,
-		categoria: String,
+		categoria: [{tipo: String, preco: String}],
 		createdOn: {type: Date, default: Date.now}
 	});
+	
+	
+	
+	
+	
+	
+	
 }
 
 module.exports.AppSchema = AppSchema;
