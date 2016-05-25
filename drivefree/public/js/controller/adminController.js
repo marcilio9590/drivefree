bde115ba4bd7ebc17eb6bffee4c91568722f9cd4
treinamento <treinamento@10.42.3.208>
2016-05-25 14:49:57 -0300

    funcao limpar

--- drivefree/public/js/controller/adminController.js
+++ drivefree/public/js/controller/adminController.js
@@ -1,41 +1,42 @@
 angular.module("app").controller("adminController", function($scope, $http){
-	
-$scope.adicionarCarro = function(){
-		
+
+	$scope.limpar = function(){
+		$scope.modelo = '';
+		$scope.ano = '';
+		$scope.placa = '';
+		$scope.cor = '';
+		$scope.categoria = '';
+	}
+
+	$scope.adicionarCarro = function(){
 		var preco = "";
 		if($scope.categoria == "A"){
 			preco = "300";
 		}
-		
 		if($scope.categoria == "B"){
 			preco = "150";
 		}
 		if($scope.categoria == "C"){
 			preco = "75";
 		}
-		
+
 		var categoria = [{tipo:$scope.categoria,preco:preco}];
-		
 		var carro = {modelo: $scope.modelo, ano: $scope.ano,placa:$scope.placa,cor: $scope.cor, categoria:categoria};
-		
 		$http.post("salvarCarro", carro, {
-			
 			headers: {'Content-Type' : 'application/json'}
-			
 		})
-		
 		.then(function(response){
-			
-			//$scope.carregarListaUsuarios();
-			
+
+			$scope.limpar();
+
 		}, function(response){
-			
+
 			alert(response.data);	
-			
+
 		});
-		
+
 	}
-	
-	
-	
+
+
+
 });
