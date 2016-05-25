88e212a3092380864ee79a5efe85b7dd6b347843
treinamento <treinamento@10.42.3.208>
2016-05-25 14:17:42 -0300

    funcao add carro

--- drivefree/public/js/controller/adminController.js
+++ drivefree/public/js/controller/adminController.js
@@ -1,3 +1,41 @@
 angular.module("app").controller("adminController", function($scope, $http){
 	
+$scope.adicionarCarro = function(){
+		
+		var preco = "";
+		if($scope.categoria == "A"){
+			preco = "300";
+		}
+		
+		if($scope.categoria == "B"){
+			preco = "150";
+		}
+		if($scope.categoria == "C"){
+			preco = "75";
+		}
+		
+		var categoria = [$scope.categoria,preco];
+		
+		var carro = {modelo: $scope.modelo, ano: $scope.ano,placa:$scope.placa,cor:$Scope.cor,categoria:categoria};
+		
+		$http.post("salvarCarro", carro, {
+			
+			headers: {'Content-Type' : 'application/json'}
+			
+		})
+		
+		.then(function(response){
+			
+			$scope.carregarListaUsuarios();
+			
+		}, function(response){
+			
+			alert(response.data);	
+			
+		});
+		
+	}
+	
+	
+	
 });
