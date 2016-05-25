6206b5f77699b8e74876934dc296db159fa83d7d
Marcilio <Marcilio@192.168.25.19>
2016-05-25 20:07:46 -0300

    comentei a de conexao do mongo e colei embaixo para uma conexao localhost

--- drivefree/server.js
+++ drivefree/server.js
@@ -11,7 +11,8 @@ var options = {
 		db: { native_parser: true },
 		server: { poolSize: 5 }
 }
-mongoose.connect('mongodb://10.42.3.205/drivefree', options);
+//mongoose.connect('mongodb://10.42.3.205/drivefree', options);
+mongoose.connect('mongodb://localhost/drivefree', options);
 
 var AppSchema = require('./schema.js').AppSchema;
 var appSchemaInstance = new AppSchema(mongoose);
