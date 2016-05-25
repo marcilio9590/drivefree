1727057db0d37d946a09d107c094890383218c94
Vinicius <treinamento@MW7586KBVIJB6B.dir.svc.accenture.com>
2016-05-25 17:04:35 -0300

    Controller da pagina cadastro

--- drivefree/public/js/controller/cadastroController.js
+++ drivefree/public/js/controller/cadastroController.js
@@ -1,3 +1,64 @@
-angular.module("app").controller("cadastroController", function($scope, $http){
+angular.module("app").controller("cadastroController", function($scope, $http, $window){
+	
+	$scope.nome = "";
+	$scope.rg = "";
+	$scope.sexo = "";
+	$scope.email = "";
+	$scope.senha = "";
+	$scope.nHabilitacao = "";
+	$scope.validadeHabilitacao = "";
+	$scope.cep = "";
+	$scope.endereco = "";
+	$scope.bairro = "";
+	$scope.cidade = "";
+	$scope.telefone = "";
+	
+	$scope.adicionarCliente = function () {
+		var cliente = {
+				
+				nome: $scope.nome,
+				rg: $scope.rg,
+				sexo: $scope.sexo,
+				email: $scope.email,
+				senha: $scope.senha,
+				nHabilitacao:  $scope.nHabilitacao,
+				validadeHabilitacao: $scope.validadeHabilitacao,
+				cep: $scope.cep,
+				endereco: $scope.endereco,
+				bairro: $scope.bairro,
+				cidade: $scope.cidade,
+				telefone: $scope.telefone
+				
+		};
+		$http.post("/salvarCliente", cliente, {
+			headers: { 'Content-Type': 'application/json'}
+		})
+		.then(
+			function(response) {
+				$window.location.href = '../../index.html';
+			},
+			function(response) {
+				alert(response.data);
+			}
+		);
+	}
+	
+	$scope.limparCampos = function(){
+		
+		$scope.nome = "";
+		$scope.rg = "";
+		$scope.sexo = "";
+		$scope.email = "";
+		$scope.senha = "";
+		$scope.nHabilitacao = "";
+		$scope.validadeHabilitacao = "";
+		$scope.cep = "";
+		$scope.endereco = "";
+		$scope.bairro = "";
+		$scope.cidade = "";
+		$scope.telefone = "";
+		
+		
+	}
 	
 });
